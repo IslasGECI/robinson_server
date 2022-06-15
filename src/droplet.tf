567b3cad061c292bc0d9cea526388a7130e78c94
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-06-14 17:13:04 -0700

    🧱 Add Terraform declarations

---
+++ src/droplet.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "robinsondata" {
+  image  = "ubuntu-22-04-x64"
+  name   = "robinsondata"
+  region = "sfo3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.evaro-desktop.id,
+    data.digitalocean_ssh_key.evaro-laptop.id,
+    data.digitalocean_ssh_key.nepo-desktop.id,
+    data.digitalocean_ssh_key.nepo-laptop.id
+  ]
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = file(var.pvt_key)
+    timeout     = "2m"
+  }
+}
+
+resource "digitalocean_floating_ip_assignment" "foobar" {
+  ip_address = "164.90.245.58"
+  droplet_id = digitalocean_droplet.robinsondata.id
+}
